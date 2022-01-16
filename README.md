bcbe11c4d344fc7a3bc823ddb704f685dfd0509d
Aaqib Javed <ajmehdi5@gmail.com>
2022-01-16 15:04:14 +0500

    initial commit

--- README.md
+++ README.md
@@ -68,3 +68,13 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+### Typography 
+just text like paragraph h1 ect
+### AppBar
+Navbar
+### Card, CardActions, CardContent, CardMedia, CssBaseLine
+### Grid, Toolbar, Container
+## import a camera icon 
+### import PhotoCamera from "@material-ui/icon";
+
--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Typography,AppBar,Card,CardActions,CardContent,CardMedia,CssBaseline,Grid,ToolBar,Container } from '@material-ui/core';
+import PhotoCamera from '@material-ui/icons';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typography variant="h1">Hello World!</Typography>
+      <Typography variant="h2">Hello World!</Typography>
+      <Typography variant="h3">Hello World!</Typography>
+      <Typography variant="h4">Hello World!</Typography>
+      <Typography variant="p">Hello World!</Typography>
+      <h1>Hello World!</h1>
     </div>
   );
 }
